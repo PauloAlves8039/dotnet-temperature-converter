66093a9ef27a86276a6d16f19657552dcdd3dc89
PauloAlves8039 <pj38alves@gmail.com>
2023-10-02 20:39:34 -0300

    Versão inicial do projeto

---
+++ src/TemperatureConverter.App/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("******************** TEMPERATURE CONVERTER ********************");
+
+        Console.ReadKey();
+    }
+}
